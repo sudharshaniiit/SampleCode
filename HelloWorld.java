36dbdcc3699d215845b618625d8138c0541f44d2
sudharshan <sudharshaniiit@gmail.com>
2020-09-06 02:45:44 +0000

    New file added

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,6 @@ class HelloWorld
  {
     public static void main(String[] args) 
 {
-        System.out.println(" Helloworld "); 
-
-	// Prints in terminal window welcome to Git
-	System.out.println(" Welcome to Git ");
+        System.out.println("Hello, World!"); 
     }
 }
