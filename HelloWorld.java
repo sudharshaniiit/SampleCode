414fb4646f6861014a8f3c66dc42fc7c34034e60
sudharshan <sudharshaniiit@gmail.com>
2020-09-06 02:18:22 +0000

    new line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ class HelloWorld
  {
     public static void main(String[] args) 
 {
-        System.out.println(" Welcome to Git "); 
+        System.out.println(" Helloworld "); 
+
+	// Prints in terminal window welcome to Git
+	System.out.println(" Welcome to Git ");
     }
 }
